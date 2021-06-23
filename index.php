156bab059e26e98f2b0610ae0b8647707e1ea956
翠花 <admin@nange.cn>
2021-06-23 16:22:48 +0800

    Update index.php

--- index.php
+++ index.php
@@ -35,18 +35,18 @@ $city_name = $city_name == $country_name ? '' : $city_name;
 // isp
 $isp = (new GeoIp2\Database\Reader('data/GeoIP2-ISP.mmdb'))->isp($ip);
 $isp_name = $isp->isp;
-$org = $isp->organization;
+#$org = $isp->organization;
 $network = $isp->network;
-$isp_org = $isp->autonomousSystemOrganization;
+#$isp_org = $isp->autonomousSystemOrganization;
 //asn
 $asn = (new GeoIp2\Database\Reader('data/GeoLite2-ASN.mmdb'))->asn($ip);
 $asn_number = $asn->autonomousSystemNumber;
-$asn_org = $asn->autonomousSystemOrganization;
+$org = $asn->autonomousSystemOrganization;
 
 // connection-type
 $connection = (new GeoIp2\Database\Reader('data/GeoIP2-Connection-Type.mmdb'))->connectionType($ip);
 $connection_type = $connection->connectionType;
-$content = array ("ip"=>"$ip","country"=>"$country_name","country_code"=>"$country_code","region"=>"$region_name","region_code"=>"$region_code","city"=>"$city_name","isp"=>"$isp_name","isp_org"=>"$isp_org","asn"=>"AS$asn_number","asn_org"=>"$asn_org","connection_type"=>"$connection_type","network"=>"$network","user_agent"=>"$ua");
+$content = array ("ip"=>"$ip","country"=>"$country_name","country_code"=>"$country_code","region"=>"$region_name","region_code"=>"$region_code","city"=>"$city_name","isp"=>"$isp_name","asn"=>"AS$asn_number","org"=>"$org","connection_type"=>"$connection_type","network"=>"$network","user_agent"=>"$ua");
 $results = json_encode($content,JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE);
 $result = stripslashes($results);
 
