1db4339e7ea9268182dea3ecfdc2f16706b12258
翠花 <admin@nange.cn>
2021-06-23 18:55:53 +0800

    Update index.php

--- index.php
+++ index.php
@@ -24,15 +24,20 @@ $ip = get_remote_addr();
 // country
 $country = (new GeoIp2\Database\Reader('data/GeoLite2-Country.mmdb'))->country($ip);
 $country_name = $country->country->names['en'];
+//$country_name = $country->country->names['zh-CN'];
 $country_code = $country->country->isoCode;
-//$city_name = $country->country->names['zh-CN'];
 //$city_name = $city_name == $country_name ? '' : $city_name;
 // city
 $city = (new GeoIp2\Database\Reader('data/GeoLite2-City.mmdb'))->city($ip);
-$region_name = $city->mostSpecificSubdivision->name;
+$region_name = $city->mostSpecificSubdivision->names['en'];
+//$region_name = $city->mostSpecificSubdivision->names['zh-CN'];
 $region_code = $city->mostSpecificSubdivision->isoCode;
 $city_name = $city->city->names['en'];
+//$city_name = $city->city->names['zh-CN'];
 $city_name = $city_name == $country_name ? '' : $city_name;
+$latitude = $city->location->latitude;
+$longitude = $city->location->longitude;
+$timezone = $city->location->timeZone;
 // isp
 $isp = (new GeoIp2\Database\Reader('data/GeoIP2-ISP.mmdb'))->isp($ip);
 $isp_name = $isp->isp;
@@ -47,8 +52,8 @@ $org = $asn->autonomousSystemOrganization;
 // connection-type
 $connection = (new GeoIp2\Database\Reader('data/GeoIP2-Connection-Type.mmdb'))->connectionType($ip);
 $connection_type = $connection->connectionType;
-$content = array ("ip"=>"$ip","country"=>"$country_name","country_code"=>"$country_code","region"=>"$region_name","region_code"=>"$region_code","city"=>"$city_name","isp"=>"$isp_name","asn"=>"AS$asn_number","org"=>"$org","connection_type"=>"$connection_type","network"=>"$network","user_agent"=>"$ua");
-$text = "$ip $country_name $country_code $region_name $region_code $city_name $isp_name AS$asn_number $org $connection_type $network $ua";
+$content = array ("ip"=>"$ip","country"=>"$country_name","country_code"=>"$country_code","region"=>"$region_name","region_code"=>"$region_code","city"=>"$city_name","latitude"=>"$latitude","longitude"=>"$longitude","time_zone"=>"$timezone","isp"=>"$isp_name","asn"=>"AS$asn_number","org"=>"$org","connection_type"=>"$connection_type","network"=>"$network","user_agent"=>"$ua");
+$text = "$ip\n $country_name\n $country_code\n $region_name\n $region_code\n $city_name\n $latitude\n $longitude\n $timezone\n $isp_name\n AS$asn_number\n $org\n $connection_type\n $network\n $ua\n";
 $result = json_encode($content,JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE);
 $json = stripslashes($result);
 
@@ -59,10 +64,10 @@ if (isset($_GET['json'])) {
 			header('content-type:application/json;charset=utf-8');
 			echo $json;
 }
-if (isset($_GET['text'])) {
+elseif  (isset($_GET['text'])) {
 			header('content-type:text/plain;charset=utf-8');
 			echo $text;
 }
-else{
-    echo $ip;
+else {
+   echo $ip;
 }
